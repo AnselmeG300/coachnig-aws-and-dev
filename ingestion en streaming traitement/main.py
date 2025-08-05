1bcbf8d7baedd9960ddfbd6abbb6fab3f0246ee9
Anselme Gildas Tchassem Boutchouang <43468327+AnselmeG300@users.noreply.github.com>
2025-08-05 17:22:52 +0100

    Create main.py

---
+++ ingestion en streaming traitement/main.py
@@ -0,0 +1,16 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+import base64
+def handler(event, context):
+
+    for record in event['Records']:
+        try:
+            print(f"Processed Kinesis Event - EventID: {record['eventID']}")
+            record_data = base64.b64decode(record['kinesis']['data']).decode('utf-8')
+            print(f"Record Data: {record_data}")
+            # TODO: Do interesting work based on the new data
+        except Exception as e:
+            print(f"An error occurred {e}")
+            raise e
+    print(f"Successfully processed {len(event['Records'])} records.")
+
